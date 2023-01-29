acaab873f4f789223b871b34fa65ad11967900d9
Sayeem2004 <MohammadSSKC@gmail.com>
2023-01-29 15:09:43 -0500

    initial commit

---
+++ assets/index.js
@@ -0,0 +1,33 @@
+// const celebs = []
+
+// window.onload = function() {
+//     console.log("Window Loaded");
+//     // load();
+// }
+
+// function start() {
+
+// }
+
+// function pause() {
+
+// }
+
+// function next() {
+
+// }
+
+// function skip() {
+
+// }
+
+// function load() {
+//     let fetchResult = fetch("./assets/celebs.csv");
+//     console.log(fetchResult);
+
+//     fetchResult.then(res => {
+//         res.text()
+//     }).then(data => {
+//         console.log(data)
+//     });
+// }
